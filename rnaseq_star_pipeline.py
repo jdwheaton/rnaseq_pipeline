d6cdd4971f2347f58e972e63d06fa11db7414125
Josh Wheaton <jwheaton16@gmail.com>
2019-02-04 20:20:22 -0500

    Initial commit, scripts should be functional

---
+++ rnaseq_star_pipeline.py
@@ -0,0 +1,124 @@
+SAMPLES, = glob_wildcards("raw_data/{smp}.fastq.gz")
+INDEX_DIR = "/work/mc394/star_index/GRCm38/"
+ANNOTATION = "/work/mc394/genomes/annotation/wgencode.vM17.primary_assembly.annotation.gtf"
+
+COUNT_FILENAME = ["results/essig2017_roquin_tfr.counts"]
+
+ALL_FASTQC = expand("fastqc_out/{smp}_fastqc.zip", smp=SAMPLES)
+ALL_BAMCOV = expand("results/{sample}.rpkm.bw", sample=SAMPLES)
+
+rule all:
+    input: ALL_BAMCOV + ALL_FASTQC + COUNT_FILENAME
+
+rule fastqc:
+    input:
+        "raw_data/{sample}.fastq.gz"
+    output:
+        "fastqc_out/{sample}_fastqc.html",
+        "fastqc_out/{sample}_fastqc.zip"
+    threads: 1
+    singularity:
+        "shub://jdwheaton/singularity-ngs:qc_trim"
+    log:
+        "logs/{sample}.fastqc.out"
+    shell:
+        "fastqc {input} -o fastqc_out/ &> {log}"
+
+rule trim_galore:
+    input:
+        "raw_data/{sample}.fastq.gz"
+    output:
+        "trimmed_fastq/{sample}_trimmed.fq"
+    threads: 1
+    singularity:
+        "shub://jdwheaton/singularity-ngs:qc_trim"
+    log:
+        "logs/{sample}.trimgalore.out"
+    shell:
+        "trim_galore --dont_gzip --fastqc -o trimmed_fastq/ {input} &> {log}"
+
+rule star_align:
+    input:
+        "trimmed_fastq/{sample}_trimmed.fq"
+    output:
+        temp("results/{sample}.Aligned.out.bam")
+    params:
+        index = INDEX_DIR,
+        prefix = "results/{sample}."
+    threads: 6
+    singularity:
+        "shub://jdwheaton/singularity-ngs:star_htseq"
+    log:
+        "logs/{sample}.star.out"
+    shell:
+        '''
+        STAR \
+        --genomeDir {params.index} \
+        --runThreadN {threads} \
+        --outFilterMultimapNmax 1 \
+        --outSAMattributes Standard \
+        --outSAMstrandField intronMotif \
+        --outSAMtype BAM Unsorted \
+        --alignSJoverhangMin 500 \
+        --readFilesIn {input} \
+        --outFileNamePrefix {params.prefix} \
+        &> {log}
+        '''
+
+rule samtools_sort:
+    input:
+        "results/{sample}.Aligned.out.bam"
+    output:
+        "results/{sample}.Aligned.out.sorted.bam"
+    threads: 3
+    singularity:
+        "shub://jdwheaton/singularity-ngs:latest"
+    log:
+        "logs/{sample}.sort.out"
+    shell:
+        "samtools sort --threads {threads} -o {output} {input} &> {log}"
+
+rule samtools_index:
+    input:
+        "results/{sample}.Aligned.out.sorted.bam"
+    output:
+        "results/{sample}.Aligned.out.sorted.bai"
+    threads: 1
+    singularity:
+        "shub://jdwheaton/singularity-ngs:latest"
+    log:
+        "logs/{sample}.index.out"
+    shell:
+        "samtools index {input} {output} &> {log}"
+
+rule featureCounts:
+    input:
+        expand("results/{sample}.Aligned.out.sorted.bam", sample=SAMPLES)
+    output:
+        COUNT_FILENAME
+    threads: 4
+    singularity:
+        "shub://jdwheaton/singularity-ngs:chip_atac_post"
+    log:
+        "logs/featureCounts.log"
+    shell:
+        "featureCounts -T {threads} -a {ANNOTATION} -o {output} {input} &> {log}"
+
+rule bam_coverage:
+    input:
+        BAM = "results/{sample}.Aligned.out.sorted.bam",
+        BAI = "results/{sample}.Aligned.out.sorted.bai"
+    output:
+        "results/{sample}.rpkm.bw"
+    singularity:
+        "shub://jdwheaton/singularity-ngs:chip_atac_post"
+    threads: 4
+    log:
+        "logs/{sample}_bamcoverage.log"
+    shell: """
+            bamCoverage -p {threads} -b {input.BAM} -o {output} \
+                --binSize 10 \
+                --normalizeUsing RPKM \
+                --ignoreForNormalization chrX \
+                &> {log}
+                """
